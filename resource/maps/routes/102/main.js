aa3aeee13bb128a759a3f66d82319eb5719fe77e
Stijn Bernards <Admin@heroicgamers.com>
2015-10-08 20:29:54 +0200

    route 102

--- resource/maps/routes/102/main.js
+++ resource/maps/routes/102/main.js
@@ -47,6 +47,90 @@ var npc = [
         ["Arrgh, I lost... I should have trained mine more..."],
         "down"
     ),
+    new pokemonCore.npc(
+        null,
+        "BUG_CATCHER_RICK",
+        ["Hahah! Our eyes met! I'll take you on with my BUG POK&eacute;MON!"],
+        null,
+        null,
+        null,
+        [26,15],
+        {
+            pokemon:[
+                {
+                    pokemon: {
+                        pokemon: new pokemonCore.pokemon.instantiate(265),
+                        level: 4,
+                    }
+                },
+            ],
+            pokemon:[
+                {
+                    pokemon: {
+                        pokemon: new pokemonCore.pokemon.instantiate(265),
+                        level: 4,
+                    }
+                },
+            ]
+        },
+        false,
+        ["BUG CATCHER RICK would like to battle!"],
+        ["Ow! Down and out!"],
+        "up"
+    ),
+    new pokemonCore.npc(
+        null,
+        "2",
+        ["I'm... not very tall, so I sink right into tall grass.", "The grass goes up my nose and... Fwafwafwafwafwa...", "Fwatchoo!"],
+        null,
+        null,
+        null,
+        [19, 11],
+        false,
+        true
+    ),
+    new pokemonCore.npc(
+        null,
+        "YOUNGSTER_ALLEN",
+        ["Did you just become a TRAINER? We're both beginners!"],
+        null,
+        null,
+        null,
+        [20,4],
+        {
+            pokemon:[
+                {
+                    pokemon: {
+                        pokemon: new pokemonCore.pokemon.instantiate(261),
+                        level: 5,
+                    }
+                },
+            ],
+            pokemon:[
+                {
+                    pokemon: {
+                        pokemon: new pokemonCore.pokemon.instantiate(276),
+                        level: 3,
+                    }
+                },
+            ]
+        },
+        false,
+        ["YOUNGSTER_ALLEN would like to battle!"],
+        ["I called you because I thought I could beat you..."],
+        "up"
+    ),
+    new pokemonCore.npc(
+        [[37, 5],[39, 3]],
+        "2",
+        ["I'm going to catch a whole bunch of POK&eacute;MON!"],
+        null,
+        null,
+        null,
+        [37, 5],
+        false,
+        true
+    ),
 ];
 
 var pokemon = [
